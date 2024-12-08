4267ee47b9caf9984119a5816cb55f1b8b950023
jpoliveira08 <joaopedro.oliveirajava@gmail.com>
2024-12-07 21:24:44 -0300

    feat_wip:Starting working on the API

---
+++ src/Modules/Contact/database/seeders/ContactDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace JpOliveira08\Contact\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class ContactDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
--- src/app-modules/contact-management/tests/ContactManagementServiceProviderTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace JpOliveira08\ContactManagement\Tests;
-
-class ContactManagementServiceProviderTest extends TestCase
-{
-	// TODO
-}
